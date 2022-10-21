1e7807f815e5a6f2af63506649aa8e5262cd7b1d
joaaoazul <116014129+joaaoazul@users.noreply.github.com>
2022-10-21 14:57:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ let earlyYears = 2;
 earlyYears *=10.5;
 let laterYears = myAge - 2;
 const myAgeInDogYears = earlyYears + laterYears;
-const myName = 'João Azul'.toLowerCase();
+const myName = 'Elaine Fred'.toLowerCase();
 
 console.log(`My name is ${myName}. Im ${myAge} years old in human years wich is ${myAgeInDogYears} years old in dog years.`)
