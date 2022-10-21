994af439124c79cb5025b3439d7da1338dee155e
joaaoazul <116014129+joaaoazul@users.noreply.github.com>
2022-10-21 14:56:57 +0100

    The Calculator file

---
+++ script.js
@@ -0,0 +1,8 @@
+const myAge = 20;
+let earlyYears = 2;
+earlyYears *=10.5;
+let laterYears = myAge - 2;
+const myAgeInDogYears = earlyYears + laterYears;
+const myName = 'João Azul'.toLowerCase();
+
+console.log(`My name is ${myName}. Im ${myAge} years old in human years wich is ${myAgeInDogYears} years old in dog years.`)
